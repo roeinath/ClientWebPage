747f32906134e0553a867a7a5c8b40fbeac86038
רואי נתנזון <T9079575@talpiot.huji.ac.il>
2023-03-28 10:44:19 +0300

    added mock server with python

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template
+from flask_cors import CORS, cross_origin
 import json
 
 CONF_FILE = 'customization.conf'
 
 app = Flask(__name__)
+
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+cors = CORS(app, resources={r"/foo": {"origins": "http://localhost:port"}})
+
+@app.route('/foo')#, methods=['POST'])
+@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
+def foo():
+    return request.json['inputVar']
 
 @app.route("/")
 def hello_world():
--- static/external_code_original.js
+++ static/external_code_original.js
@@ -1,71 +1,12 @@
-let unwanted_os = ["windows", "os x", "ios"];
+const CLIENT_VERSION_MAJOR = 1;
+const CLIENT_VERSION_MINOR = 1;
 
-for (var os of unwanted_os) {
-    if (platform.os.toString().toLowerCase().includes(os)) {
-        document.getElementById("do_fingerprint_btn").disabled = true;
-        poc_log("ERROR: Tracking can be made with Linux-based devices only");
-        break;
-    }
-}
+poc_log(`Client version: v${CLIENT_VERSION_MAJOR}.${CLIENT_VERSION_MINOR}`)
 
-async function fill_in_form() {
-    document.getElementById("browser").value = platform.name;
-    document.getElementById("os").value = platform.os;
-}
-
-fill_in_form();
-
-function tracker_address_select_onchange() {
-    let select_tag = document.getElementById("tracker_address_select");
-    let selected_option = select_tag.options[select_tag.selectedIndex];
-    let tracker_address_input = document.getElementById("tracker_address");
-    if (selected_option.text == "Custom") {
-        tracker_address_input.value = "";
-    } else if (selected_option.text == "Self") {
-        tracker_address_input.value = window.location.hostname;
-    } else {
-        tracker_address_input.value = selected_option.value;
-    }
-}
-
-tracker_address_select_onchange();
-
-const LOOPBACK_IPV4_ADDR = '127.0.0.1';
-
-function poc_log(text) {
-    let today = new Date();
-    let date = `${today.getFullYear()}-${String(today.getMonth()+1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
-    let time = `${String(today.getHours()).padStart(2, '0')}:${String(today.getMinutes()).padStart(2, '0')}:${String(today.getSeconds()).padStart(2, '0')},${String(today.getMilliseconds()).padStart(3,'0')}`;
-    let elem = document.getElementById("poc_log");
-    elem.innerText += `${date} ${time}  ${text}\n`;
-    console.log(`${date} ${time}  ${text}\n`);
-    elem.scrollTop = elem.scrollHeight;
-}
-
-function clear_poc_log() {
-    let elem = document.getElementById("poc_log");
-    elem.innerText = "";
-}
-
-function set_intersection(a, b) {
-    return new Set([...a].filter(x => b.has(x)));
-}
-
-function set_union(a, b) {
-    return new Set([...a, ...b]);
-}
-
-function set_length(a) {
-    return a.size;
-}
-
-function randstring(length) {
-    let result = "";
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    for (let i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    return result;
+let batch_tcp_connect_func = batch_tcp_connect_xhr;
+if (platform.name == "Chrome" || platform.name == "Chrome Mobile"){
+    poc_log("Chrome browser detected");
+    batch_tcp_connect_func = batch_tcp_connect_webrtc;
 }
 
 function batch_tcp_connect_xhr(daddr, dports) {
@@ -139,9 +80,6 @@ function do_batch_tcp_connect_webrtc(host, dports) {
     });
 }
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
 
 function batch_tcp_connect_webrtc(daddr, dports) {
     return Promise.race([do_batch_tcp_connect_webrtc(daddr, dports),sleep(1)]);
